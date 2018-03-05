c01ea90f1555fdb4092d8648dee2ac387186a6b5
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 17:42:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map! {|call| call + "!"}
+  planeteer_calls.map! {|call| call.capitalize + "!"}
   planeteer_calls.each {|call|
     call.capitalize
   }
