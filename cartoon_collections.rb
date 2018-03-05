8cbe7019754b19292166f5ebfbbb7c6565ba2559
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 17:38:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,13 @@
 def roll_call_dwarves(array)
   array.each_with_index { |name, index|
     puts "#{index+1}. #{name}"
-  }# code an argument here
-  # Your code here
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.each {|call|
+  call.upcase
+}
 end
 
 def long_planeteer_calls# code an argument here
