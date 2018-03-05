cc64d5fc381413c6336de87e81d0c41b678951ef
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 17:48:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,14 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|call| call.capitalize + "!"}
-#  planeteer_calls.each {|call|
-#    call.capitalize
-#  }
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)
+  long_planeteer_calls.each do |call|
+    return true if call.length > 4
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
