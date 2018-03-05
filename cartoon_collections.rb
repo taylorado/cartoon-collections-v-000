4e6aac9fc24a90a4c6577d01d0a70bd463fc54cf
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 17:34:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index |name, index|
+    puts name
+  end# code an argument here
   # Your code here
 end
 
